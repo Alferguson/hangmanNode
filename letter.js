583b89758a212db6322a7bc899813f317fda359a
Alferguson <alferguson@berkeley.edu>
2017-11-09 18:44:28 -0800

    final

--- letter.js
+++ letter.js
@@ -1,20 +1,15 @@
 var Word = require("./word.js");
-
-// do prototypes for these in letter
-
-// constructor function to match letters in current word
 var inquirer = require("inquirer");
 var wordsArr = ["dingus", "mongolia", "pump-up-the-jam", "pneumonoultramicroscopicsilicovolcanoconiosis"];
-
 var guesses = 10;
+
 console.log("Welcome to hangmanNode! To keep you guessing, there are no spaces between blanks so you can't guess how many characters are left (;")
 
 var Letter = function() {
-
-
-	this.newWord = function(i) {
-
-		var newWord = new Word(i);
+	this.newWord = function(num) {
+		// to grab constructor from Word
+		var newWord = new Word(num);
+		// function so pump-up-the-jam's '-' doesn't have to be guessed by user
 		var blanksFunc = function() {
 			for (var i=0; i<newWord.guessWord.length; i++) {
 				if (newWord.guessWord[i] === "-") {
@@ -23,12 +18,13 @@ var Letter = function() {
 					newWord.blanks = blanksArr.join("");
 				}
 			}
-		}			
+		}
 		blanksFunc();		
-
+		// console logs blanks for the word
 		console.log(newWord.blanks);
 
 		var inquire = function() {
+			// oldblanks is made so that a comparision can be made later
 			oldBlanks = newWord.blanks;
 			inquirer.prompt([
 
@@ -38,19 +34,18 @@ var Letter = function() {
 			    message: "Guess a letter "
 			  },
 
-			 
 			]).then(function(user) {
+				// for loop to go through word and compare with user guess and then change blank lines accordingly
 				for (var j=0; j < newWord.guessWord.length; j++) {
 					if (user.userLetter.toLowerCase() == newWord.guessWord[j]) {
 						blanksArr = newWord.blanks.split("");
 						blanksArr[j] = user.userLetter.toLowerCase();
 						newWord.blanks = blanksArr.join("");
-
-
 					}
-
 				}
+				// console.log new blanks with changes
 				console.log(newWord.blanks);
+				// if user guess was wrong and there are no more guesses
 				if (oldBlanks == newWord.blanks) {
 					guesses--;
 					if (guesses === 0) {
@@ -58,8 +53,9 @@ var Letter = function() {
 						return;
 					}						
 					console.log("That was incorrect, you have " + guesses + " remaining");
+					// run inquire again
 					inquire();
-
+				// if you completed the word
 				}
 				else if (newWord.blanks == newWord.guessWord) {
 					i++;
@@ -71,17 +67,17 @@ var Letter = function() {
 					console.log("You got the word, onto the next one");
 					newLetter.newWord(i);
 				}
+				// if old blanks is not equal to the new blanks, then you have guessed correctly
 				else if (oldBlanks != newWord.blanks) {
 					console.log("You have guessed correctly!");
 					inquire();
 				}
 			});
-
 		}
+		// run inquire if this.newWord is run
 		inquire();
 	}
-
 }
 var newLetter = new Letter();
+// run this when code is run
 newLetter.newWord(0);
-
--- word.js
+++ word.js
@@ -1,6 +1,5 @@
 var wordsArr = ["dingus", "mongolia", "pump-up-the-jam", "pneumonoultramicroscopicsilicovolcanoconiosis"];
 var wordCounter = 0;
-
 var blank = "_";
 
 // constructor function to create current guess word
@@ -8,14 +7,7 @@ var Word = function(guessWordNumber) {
 	this.guessWord = wordsArr[guessWordNumber];
 	this.blanks = blank.repeat(this.guessWord.length);
 }
+// prototype wordsArr so I don't have to reclassify wordsArr in the other javascript but only works with first word and not after
 Word.prototype.wordsArr = wordsArr;
-
-
-// var newWord = new Word(1);
-
-// console.log(newWord.wordsArr);
-// console.log(wordsArr[1])
-// console.log(newWord.blanks);
-// console.log(newWord.blanks[1]);
 
 module.exports = Word;
