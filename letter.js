e1209dc1f32b63613e4f5173b16aead9dbfb8205
Alferguson <alferguson@berkeley.edu>
2017-11-09 18:21:11 -0800

    Basically done, needs to be cleaned up and noted

--- letter.js
+++ letter.js
@@ -4,7 +4,7 @@ var Word = require("./word.js");
 
 // constructor function to match letters in current word
 var inquirer = require("inquirer");
-var wordsArr = ["Dingus", "Mongolia", "Pump-up-the-jam", "Pneumonoultramicroscopicsilicovolcanoconiosis"];
+var wordsArr = ["dingus", "mongolia", "pump-up-the-jam", "pneumonoultramicroscopicsilicovolcanoconiosis"];
 
 var guesses = 10;
 console.log("Welcome to hangmanNode! To keep you guessing, there are no spaces between blanks so you can't guess how many characters are left (;")
@@ -12,66 +12,74 @@ console.log("Welcome to hangmanNode! To keep you guessing, there are no spaces b
 var Letter = function() {
 
 
-		this.newWord = function(i) {
+	this.newWord = function(i) {
 
-			var newWord = new Word(i);
-
-
-			console.log(newWord.blanks);
-
-
-			var inquire = function() {
-				oldBlanks = newWord.blanks;
-				inquirer.prompt([
-
-				  {
-				    type: "input",
-				    name: "userLetter",
-				    message: "Guess a letter "
-				  },
-
-				 
-				]).then(function(user) {
-					for (var j=0; j < newWord.guessWord.length; j++) {
-						if (user.userLetter == newWord.guessWord[j]) {
-							blanksArr = newWord.blanks.split("");
-							blanksArr[j] = user.userLetter;
-							newWord.blanks = blanksArr.join("");
-
-
-						}
-
-					}
-					console.log(newWord.blanks);
-					if (oldBlanks == newWord.blanks) {
-						guesses--;
-						if (guesses === 0) {
-							console.log("Sorry sucka, but you lose");
-							return;
-						}						
-						console.log("That was incorrect, you have " + guesses + " remaining");
-						inquire();
-
-					}
-					else if (newWord.blanks == newWord.guessWord) {
-						i++;
-						guesses = 10;
-						if (i === wordsArr.length) {
-							console.log("You have won!")
-							return;
-						}
-						console.log("You got the word, onto the next one");
-						newLetter.newWord(i);
-					}
-					else if (oldBlanks != newWord.blanks) {
-						console.log("You have guessed correctly!");
-						inquire();
-					}
-				});
-
+		var newWord = new Word(i);
+		var blanksFunc = function() {
+			for (var i=0; i<newWord.guessWord.length; i++) {
+				if (newWord.guessWord[i] === "-") {
+					blanksArr = newWord.blanks.split("");
+					blanksArr[i] = "-";
+					newWord.blanks = blanksArr.join("");
+				}
 			}
-			inquire();
+		}			
+		blanksFunc();		
+
+		console.log(newWord.blanks);
+
+		var inquire = function() {
+			oldBlanks = newWord.blanks;
+			inquirer.prompt([
+
+			  {
+			    type: "input",
+			    name: "userLetter",
+			    message: "Guess a letter "
+			  },
+
+			 
+			]).then(function(user) {
+				for (var j=0; j < newWord.guessWord.length; j++) {
+					if (user.userLetter.toLowerCase() == newWord.guessWord[j]) {
+						blanksArr = newWord.blanks.split("");
+						blanksArr[j] = user.userLetter.toLowerCase();
+						newWord.blanks = blanksArr.join("");
+
+
+					}
+
+				}
+				console.log(newWord.blanks);
+				if (oldBlanks == newWord.blanks) {
+					guesses--;
+					if (guesses === 0) {
+						console.log("Sorry sucka, but you lose");
+						return;
+					}						
+					console.log("That was incorrect, you have " + guesses + " remaining");
+					inquire();
+
+				}
+				else if (newWord.blanks == newWord.guessWord) {
+					i++;
+					guesses = 10;
+					if (i === wordsArr.length) {
+						console.log("You have won!")
+						return;
+					}
+					console.log("You got the word, onto the next one");
+					newLetter.newWord(i);
+				}
+				else if (oldBlanks != newWord.blanks) {
+					console.log("You have guessed correctly!");
+					inquire();
+				}
+			});
+
 		}
+		inquire();
+	}
 
 }
 var newLetter = new Letter();
--- word.js
+++ word.js
@@ -1,5 +1,4 @@
-var wordsArr = ["Dingus", "Mongolia", "Pump-up-the-jam", "Pneumonoultramicroscopicsilicovolcanoconiosis"]
-
+var wordsArr = ["dingus", "mongolia", "pump-up-the-jam", "pneumonoultramicroscopicsilicovolcanoconiosis"];
 var wordCounter = 0;
 
 var blank = "_";
@@ -9,10 +8,12 @@ var Word = function(guessWordNumber) {
 	this.guessWord = wordsArr[guessWordNumber];
 	this.blanks = blank.repeat(this.guessWord.length);
 }
+Word.prototype.wordsArr = wordsArr;
+
 
 // var newWord = new Word(1);
 
-// console.log(newWord.guessWord);
+// console.log(newWord.wordsArr);
 // console.log(wordsArr[1])
 // console.log(newWord.blanks);
 // console.log(newWord.blanks[1]);
